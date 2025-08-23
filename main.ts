8a1d91b97b2e290bdbc47d6e97439e56be2b0cb8
Osama Altaf <osama.altaf385@gmail.com>
2025-08-24 02:25:39 +0500

    test

--- main.ts
+++ main.ts
@@ -8,14 +8,55 @@ const server = new McpServer({
   version: "0.0.1",
   description: "A simple weather MCP server"});
 
-  server.tool("getCurrentWeather", 'Tool to get the weather of a city' , {
-    city: z.string().describe("The city to get the weather for")
+//   server.tool("getCurrentWeather", 'Tool to get the weather of a city' , {
+//     city: z.string().describe("The city to get the weather for")
+//   },
+// async ({city}) => {
+//   // In a real implementation, you would fetch the weather from an API
+//   // Here, we return a mock response
+//   return `The current weather in ${city} is sunny with a temperature of 25°C.`;
+// });
+
+server.tool(
+  'get-weather',
+  'Tool to get the weather for a city',
+  {
+    city: z.string().describe('The name of the city to get the weather for'),
   },
-async ({city}) => {
-  // In a real implementation, you would fetch the weather from an API
-  // Here, we return a mock response
-  return `The current weather in ${city} is sunny with a temperature of 25°C.`;
-});
+  async ({ city }) => {
+    // get coordinates for the city
+    const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`);
+    const data = await response.json();
+
+    // handle city not found
+    if (data.results.length === 0) {
+      return {
+        content: [
+          {
+            type: 'text',
+            text: `City ${city} not found.`,
+          }
+        ]
+      }
+    }
+
+    // get the weather data using the coordinates
+    const { latitude, longitude } = data.results[0];
+
+    const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation,rain,showers,cloud_cover,apparent_temperature`)
+
+    const weatherData = await weatherResponse.json();
+
+    return {
+      content: [
+        {
+          type: 'text',
+          text: JSON.stringify(weatherData, null, 2),
+        }
+      ]
+    }
+  }
+);
 
 const transport = new StdioServerTransport();
 server.connect(transport);
